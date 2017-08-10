ca3c40faaf02c8dede5b9bb7bd091f77f6dfe1b3
rakshitshah7 <rakshitshah7@gmail.com>
2017-08-09 23:01:40 -0400

    Changes for UserProfile and NewServices

--- public/views/product/component/productcardComponent.js
+++ public/views/product/component/productcardComponent.js
@@ -1,10 +1,35 @@
 (function () {
-        function productcardController(toaster) {
+        function productcardController(toaster, $rootScope) {
             var ctrl = this;
 
-            ctrl.addtocart = function() {
+            ctrl.addtocart = function () {
                 toaster.pop('info', "Information", "Item Added to the cart!! Check Cart");
                 ctrl.addtoCart({product: ctrl.data});
+            };
+
+            ctrl.togglefav = function (product) {
+                if ($rootScope != null) {
+                    if ($rootScope.favList === undefined) {
+                        $rootScope.favList = [];
+                    }
+
+                    var productIndex = $.map($rootScope.favList, function (obj, index) {
+                        if (obj.productID == product.productID) {
+                            return index;
+                        }
+                    });
+                    if (productIndex > -1 && $rootScope.favList.length !=0 ) {
+                        toaster.pop('info', "Information", "Removed from favourite list");
+                        $rootScope.favList.splice(productIndex, 1);
+                    }
+                    else {
+                        toaster.pop('info', "Information", "Item Added to your favourites!!");
+                        $rootScope.favList.push(ctrl.data);
+                    }
+                }
+                else {
+                    toaster.pop('error', "Information", "Log In to add");
+                }
             };
         }
 
@@ -14,13 +39,12 @@
 
         function productCard() {
 
-            return{
+            return {
                 templateUrl: "views/product/templates/productCard.html",
-                bindings: { data: '=', addtoCart: '&'   },
+                bindings: {data: '=', addtoCart: '&'},
                 controller: productcardController,
             };
 
         }
 
-}
-)();
+    })();
--- public/views/user/controllers/userController.js
+++ public/views/user/controllers/userController.js
@@ -3,10 +3,56 @@
             .module("project")
             .controller("userController", userController);
 
-        function userController($location) {
+        function userController($location, $rootScope, OrderService, UserService) {
             var vm = this;
+            vm.currentUser = {};
+            vm.favList = [];
+            vm.orderList = [];
+            vm.getUserOrders = getUserOrders;
+            vm.cancelOrder = cancelOrders;
+
+            function getUserOrders() {
+
+                OrderService
+                    .getUserOrders(currentUser)
+                    .then(function (response) {
+                            vm.orderList = response.data;
+                        },
+                        function (error) {
+                            vm.error = "Invalid Credentials"
+                        });
+
+            }
+
+            function cancelOrder(order) {
+
+                OrderService
+                    .cancelUserOrder(order)
+                    .then(function (response) {
+
+                            //vm.orderList = response.data;
+                        },
+                        function (error) {
+                            vm.error = "Invalid Credentials"
+                        });
+
+            }
+
+            $(".btn-pref .btn").click(function () {
+                $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+                // $(".tab").addClass("active"); // instead of this do the below
+                $(this).removeClass("btn-default").addClass("btn-primary");
+            });
 
             function init() {
+                if ($rootScope.favList === undefined) {
+                    $rootScope.favList = [];
+                }
+                if ($rootScope.currentUser === undefined) {
+                    $rootScope.currentUser = {};
+                }
+                vm.currentUser = $rootScope.currentUser;
+                vm.favList = $rootScope.favList;
                 console.log("In userController")
             }
 
