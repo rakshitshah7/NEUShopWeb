e45309f420654143dc8213c714213d5aa081f797
vishal6557 <vishal65.diyora@gmail.com>
2017-08-09 21:52:39 -0400

    removing database

--- server/models/user/database.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-var q= require("q");
-
-var connectionString = 'mongodb://localhost/neutailor';
-var db = mongoose.connect(connectionString);
-
-mongoose.Promise = q.Promise;
-
-module.exports = db;
