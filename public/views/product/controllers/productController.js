789de4754359c7f26809b2bb1240915fdf8fc5c8
rakshitshah7 <rakshitshah7@gmail.com>
2017-08-12 08:48:28 -0400

    Update productController.js

--- public/views/product/controllers/productController.js
+++ public/views/product/controllers/productController.js
@@ -8,6 +8,7 @@
 
             ///vm.product = [{productID: 1,productName: "Converse Shoes", description: "Chuck Taylor All Star Classic Colours",img: "https://www.converse.com/on/demandware.static/-/Sites-ConverseMaster/default/dw48f5fc3c/images/hi-res/M9621C_standard.png?sw=580&sh=580&sm=fit", edition: "Classic red converse edition", color:"Red", styleNo:"M9621C", costPrice:75 , sellingPrice:50 },{productID: 2,productName: "Converse Shoes", description: "Chuck Taylor All Star Classic Colours",img: "https://www.converse.com/on/demandware.static/-/Sites-ConverseMaster/default/dwec469ff7/images/hi-res/151116C_standard.jpg?sw=580&sh=580&sm=fit", edition: "Classic Neon converse edition", color:"Neon", styleNo:"M9631C", costPrice:45 , sellingPrice:40 }];
             //vm.filteredList = vm.product;
+            vm.product = [];
             vm.filterProducts = filterProducts;
             vm.searchString = "";
             vm.criterias = ["Product Name","Description","Editon","Color","Style","Price"];
@@ -65,7 +66,23 @@
             function getProducts() {
                 ProductService.getProducts()
                     .then(function (response) {
-                            vm.product = response.data;
+
+                        for(var i =0;i<response.data.length;i++)
+                        {
+                            var productObj = {
+                                productID: response.data[i].productID,
+                                productName: response.data[i].productName,
+                                description: response.data[i].description,
+                                img: response.data[i].img,
+                                edition: response.data[i].edition,
+                                color: response.data[i].color,
+                                styleNo: response.data[i].styleNo,
+                                costPrice: response.data[i].costPrice,
+                                sellingPrice: response.data[i].sellingPrice
+                            }
+                            vm.product.push(productObj);
+                        }
+
                             vm.filteredList = vm.product;
                             //vm.product = response.data;
                         },
